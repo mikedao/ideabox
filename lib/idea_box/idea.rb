c1cb431bbd11b355fef140cd70cc8328565bf7cc
Michael Dao <michael.dao@gmail.com>
2014-12-16 18:16:20 -0700

    I9: Ranking and Sorting Complete

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -1,22 +1,42 @@
 
 class Idea
+	include Comparable
 	attr_reader :title, 
-							:description
+							:description,
+							:rank, 
+							:id
 
 	def initialize(attributes)
 		@title 				= attributes["title"]
 		@description 	= attributes["description"]
+		@rank 				= attributes["rank"] || 0
+		@id 					= attributes["id"]
 	end
 
 	def save
-		database.transaction do
-			database['ideas'] ||= []
-			database['ideas'] << {"title" => title, "description" => description}
-		end
+		IdeaStore.create(to_h)
+	end
+
+	def to_h
+		{
+			"title" 			=> title, 
+			"description" => description,
+			"rank" 				=> rank
+		}
 	end
 
 	def database
 		Idea.database
 	end
+
+	def like!
+		@rank += 1
+	end
+
+	def <=>(other)
+		other.rank <=> rank
+	end
+
+
 
 end
--- lib/idea_box/idea_store.rb
+++ lib/idea_box/idea_store.rb
@@ -1,10 +1,13 @@
 require 'yaml/store'
 
 class IdeaStore
+	
 	def self.all
-		raw_ideas.map do |data|
-			Idea.new(data)
+		ideas = []
+		raw_ideas.each_with_index do |data, i|
+			ideas << Idea.new(data.merge("id" => i))
 		end
+		ideas
 	end
 
 	def self.raw_ideas
@@ -14,7 +17,13 @@ class IdeaStore
 	end
 
 	def self.database
+		return @database if @database
+
 		@database ||= YAML::Store.new('db/ideabox')
+		@database.transaction do
+			@database['ideas'] ||= []
+		end
+		@database
 	end
 
 	def self.delete(position)
@@ -24,7 +33,8 @@ class IdeaStore
 	end
 
 	def self.find(id)
-		Idea.new(find_raw_idea(id))
+		raw_idea = find_raw_idea(id)
+		Idea.new(raw_idea.merge("id" => id))
 	end
 
 	def self.find_raw_idea(id)
@@ -39,10 +49,9 @@ class IdeaStore
 		end
 	end
 
-	def self.create(attributes)
+	def self.create(data)
 		database.transaction do
-			database['ideas'] ||= []
-			database['ideas'] << attributes
+			database['ideas'] << data
 		end
 	end
 end
